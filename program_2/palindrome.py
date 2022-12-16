471ed22ceffb16ca01ab8835a0d42d6b238722ff
Olga <shelepenko5@gmail.com>
2022-12-16 23:17:37 +0200

    Вправи за типами даних str

---
+++ program_2/palindrome.py
@@ -0,0 +1,12 @@
+# Программа, що перевіряє чи є введена строка дзеркальною(паліндромом)
+# введення даних
+info = str(input('Введіть текст або цифриє: '))
+# переводимо отримані данні в нижній реєстр і видаляємо пробіли
+info = info.lower().replace(' ','').replace('.','').replace(',','').replace('-','').replace(':','').replace(';','').replace('?','').replace('!','').replace('"','').replace("'",'')
+# перевертаємо отримані дані
+info_new = info[::-1]
+# порівнюємо отримані та перевернуті дані
+if info == info_new:
+    print('It is a palindrome')
+else:
+    print('It is NOT a palindrome')
