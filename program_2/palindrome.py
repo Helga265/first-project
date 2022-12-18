1a1c991e127b087ece8cded5d03d83ea28c0f721
Olga <shelepenko5@gmail.com>
2022-12-18 20:59:27 +0200

    Вправи за типами даних str

--- program_2/palindrome.py
+++ program_2/palindrome.py
@@ -1,8 +1,9 @@
 # Программа, що перевіряє чи є введена строка дзеркальною(паліндромом)
 # введення даних
-info = str(input('Введіть текст або цифру: '))
+info = str(input('Введіть текст або цифри: '))
 # переводимо отримані данні в нижній реєстр і видаляємо пробіли
-info = info.lower().replace(' ','').replace('.','').replace(',','').replace('-','').replace(':','').replace(';','').replace('?','').replace('!','').replace('"','').replace("'",'')
+info = info.lower().replace(' ', '').replace('.', '').replace(',', '').replace('-', '').replace(':', '')\
+    .replace(';', '').replace('?', '').replace('!', '').replace('"', '').replace("'", '')
 # перевертаємо отримані дані
 info_new = info[::-1]
 # порівнюємо отримані та перевернуті дані
