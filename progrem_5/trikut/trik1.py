03873dd8dd48ee198c4d0ef237d8e529eeedff45
Olga <shelepenko5@gmail.com>
2023-01-02 11:24:36 +0200

    Програма перевіряє чи існує трикутник по введеним сторонам та рахує периметр і площу

---
+++ progrem_5/trikut/trik1.py
@@ -0,0 +1,22 @@
+def cile(a, b, c) -> tuple:
+    if a > 0 and b > 0 and c > 0:
+        return a, b, c
+    else:
+        print('Значення повинні бути більше 0!')
+
+
+def trik(a, b, c) -> bool:
+    if a + b > c and b + c > a and a + c > b:
+        return True
+    else:
+        return False
+
+
+def perimt(a, b, c) -> float:
+    p = (a + b + c) / 2
+    return p
+
+
+def plosh(perimeter, a, b, c) -> float:
+    square = pow(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c), 1 / 2)
+    return round(square, 2)
